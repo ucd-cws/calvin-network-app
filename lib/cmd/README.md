f29cbc7e2accc00ded7d2c7befd5158b1c219444
Justin Merz <jrmerz@gmail.com>
2015-09-14 12:21:49 -0700

    removing bin, adding edit node properties cmd

--- lib/cmd/README.md
+++ lib/cmd/README.md
@@ -2,7 +2,7 @@
 
 /lib/cmd let's you interact with the app's models.  You have access to all models and their functions.
 
-#### Commands
+#### Overview
 
 Print all available models and functions.  Ignore the callback parameter, it's not required.
 ```
@@ -15,3 +15,36 @@ node lib/cmd [-m | --model] [model_name] [-f | --function] [function_name] param
 ```
 
 Any parameter that 'looks like' JSON will be parsed as such.
+
+## Functions
+
+#### Edit Node Location
+Edit location point geometry of a node.
+
+- Path: full path to data repo or '.'  If '.' is used the script will assume the data repo and the app repo share the same parent folder.
+- Prmname: name of the node you wish to edit
+- lnglat: Longitute and latitude of new point.
+- reverse: if you are suppling the new point as latitude,longitute pass this flag as true and the script will reverse.
+
+
+```
+node lib/cmd -m node -f editLocation [path] [prmname] [lnglat] [reverse]
+
+// ex:
+node lib/cmd -m node -f editLocation . C87 38.581517,-121.708689 true
+```
+
+#### Edit Node Properties
+Edit location point geometry of a node.
+
+- Path: full path to data repo or '.'  If '.' is used the script will assume the data repo and the app repo share the same parent folder.
+- Prmname: name of the node you wish to edit
+- properties: JSON object containing properties you wish to update
+
+
+```
+node lib/cmd -m node -f editLocation [path] [prmname] [properties]
+
+// ex:
+node lib/cmd -m node -f editLocation . C87 "{description: 'my new update'}"
+```
--- models/node/index.js
+++ models/node/index.js
@@ -5,21 +5,32 @@ var fs = require('fs');
 
 module.exports = function() {
     return {
-        editLocation : editLocation
+        editLocation : editLocation,
+        editProperties : editProperties
     };
 };
+
+//node cmd -m node -f edit_properties sr_sac/u202 {description:"This is new"}
+function editProperties(dataRepoRootPath, prmname, properties, callback) {
+  getNode(dataRepoRootPath, prmname, function(err, path, node){
+    if( !node.properties ) {
+      node.properties = {};
+    }
+
+    for( var key in properties ) {
+      node.properties[key] = properties[key];
+    }
+
+    fs.writeFileSync(path, JSON.stringify(node, '  ', '  '));
+    callback(null, 'success!\n'+path);
+  });
+}
 
 // actually update a nodes location
 function editLocation(dataRepoRootPath, prmname, latlng, reverse, callback) {
   if( typeof reverse === 'function' ) {
     callback = reverse;
     reverse = false;
-  }
-
-  prmname = prmname.toLowerCase();
-
-  if( dataRepoRootPath === '.' ) {
-    dataRepoRootPath = path.join(__dirname,'..','..','..','calvin-network-data','data');
   }
 
   if( typeof latlng === 'string' ){
@@ -31,20 +42,37 @@ function editLocation(dataRepoRootPath, prmname, latlng, reverse, callback) {
     latlng = [latlng[1], latlng[0]];
   }
 
+  getNode(dataRepoRootPath, prmname, function(err, path, node){
+    if( err ) {
+      return callback(err);
+    }
+
+    node.geometry.coordinates = latlng;
+
+    fs.writeFileSync(path, JSON.stringify(node, '  ', '  '));
+    callback(null, 'success!\n'+path);
+  });
+}
+
+function getNode(dataRepoRootPath, prmname, callback) {
+  prmname = prmname.toLowerCase();
+
+  if( dataRepoRootPath === '.' ) {
+    dataRepoRootPath = path.join(__dirname,'..','..','..','calvin-network-data','data');
+  }
+
   var walk = finder(dataRepoRootPath);
   walk.on('directory', function (dir, stat, stop) {
       var base = path.basename(dir).toLowerCase();
 
       if (base === prmname ) {
         base = path.join(dir, 'node.geojson');
-        var node = eval('('+fs.readFileSync(base, 'utf-8')+')');
-        node.geometry.coordinates = latlng;
 
-        fs.writeFileSync(base, JSON.stringify(node, '  ', '  '));
+        var node = eval('('+fs.readFileSync(base, 'utf-8')+')');
+        callback(null, base, node);
 
         stop();
         walk.stop();
-        callback(null, 'success!\n'+base);
       }
   });
 
