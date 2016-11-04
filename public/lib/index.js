001733bde252adc492b68c773e5eccb9fcb5fe3e
Justin Merz <jrmerz@gmail.com>
2016-11-04 14:52:34 -0700

    new version of HNF, fix for loading in FF

--- public/lib/index.js
+++ public/lib/index.js
@@ -1,10 +1,16 @@
 
 require('./sigma-cwn-plugin');
 
-module.exports = {
+var api = {
   collections : require('./collections'),
   controllers : require('./controllers'),
   map : require('./map'),
   renderer : require('./renderer'),
   chartLoadHandlers : require('./charts')
 }
+
+module.exports = api;
+
+$(document).ready(function(){
+    api.controllers.network.load();
+});
